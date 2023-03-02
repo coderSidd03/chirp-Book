33df9e796de0636703eee30eb0a307f33cce0af5
soumya <97720811+coderSidd03@users.noreply.github.com>
2023-03-02 21:21:34 +0530

    frontEnd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css"
+import "./App.css";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/home/Home";
-import Profile from './pages/Profile/Profile.jsx'
+import Profile from "./pages/Profile/Profile.jsx";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 // import Chat from "./pages/Chat/Chat.jsx"
@@ -10,20 +10,32 @@ function App() {
   const user = useSelector((state) => state.authReducer.authData);
 
   return (
-    <div className="App" style={{
-      height:
-        window.location.href === "http://localhost:3000/chat"
-          ? "calc(100vh - 2rem)"
-          : "auto",
-    }}>
-      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
-      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
+    <div
+      className="App"
+      style={{
+        height: "auto",
+      }}
+    >
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
 
       <Routes>
-        <Route path="/" element={user ? <Navigate to="home" /> : <Navigate to="auth" />} />
-        <Route path="/home" element={user ? <Home /> : <Navigate to="../auth" />} />
-        <Route path="/auth" element={user ? <Navigate to="../home" /> : <Auth />} />
-        <Route path="/profile/:id" element={user ? <Profile /> : < Navigate to="../auth" />} />
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+        <Route
+          path="/profile/:id"
+          element={user ? <Profile /> : <Navigate to="../auth" />}
+        />
         <Route
           path="*"
           element={
@@ -32,9 +44,8 @@ function App() {
             </main>
           }
         />
-
       </Routes>
-    </div >
+    </div>
   );
 }
 
